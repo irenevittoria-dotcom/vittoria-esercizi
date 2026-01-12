da0df0739bd5905dd70f8680321b2fef0143c061
irenevittoria-dotcom <irenevittoria2@gmail.com>
2026-01-12 13:07:44 +0100

    Add files via upload

---
+++ Turtle.py
@@ -0,0 +1,45 @@
+import turtle
+import random
+
+screen = turtle.Screen()
+screen.setup(800, 800)
+screen.bgcolor("white")
+
+t = turtle.Turtle()
+t.hideturtle()
+t.speed(0)
+turtle.tracer(0)
+
+def draw_rhombus(x, y, side, angle, rotation, color):
+    t.penup()
+    t.goto(x, y)
+    t.setheading(rotation)
+    t.pendown()
+
+    t.fillcolor(color)
+    t.begin_fill()
+
+    for i in range(2):
+        t.forward(side)
+        t.left(angle)
+        t.forward(side)
+        t.left(angle)
+
+    t.end_fill()
+    t.penup()
+
+for j in range(50):
+    x = random.uniform(-300, 300)
+    y = random.uniform(-300, 300)
+    side = random.uniform(40, 120)
+    angle = random.uniform(30, 150)
+    rotation = random.uniform(0, 360)
+    color = (random.random(), random.random(), random.random())
+
+    draw_rhombus(x, y, side, angle, rotation, color)
+
+turtle.tracer(1)
+turtle.done()
+
+    
+
